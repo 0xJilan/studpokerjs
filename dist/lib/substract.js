b4a951eb3c46c57aa0bde6a87025d59b17803133
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-13 09:26:25 +0200

    run the first build before publish

---
+++ dist/lib/substract.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.substract = void 0;
+/**
+ * Simple function to help substract two values between them
+ * @param {number} first First number to substract
+ * @param {number} second Second number substract to the first
+ * @returns  The second number subtracted from the first
+ * @exemple
+ * substract(10 - 100)
+ * // => 90
+ */
+const substract = (first, second) => {
+    return first - second;
+};
+exports.substract = substract;
+//# sourceMappingURL=substract.js.map
