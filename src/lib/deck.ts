9dd90434492ec56e83f387e37d6036a5bb66701e
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-16 08:53:23 +0200

    create shuffle deck function

--- src/lib/deck.ts
+++ src/lib/deck.ts
@@ -14,11 +14,13 @@ const ranksOfCards: string[] = [
   "K",
   "A",
 ];
-
 export const createDeckOfCards = (): string[] => {
   let newDeck: string[] = [];
   suitsOfCards.map((suit) =>
     ranksOfCards.map((rank) => newDeck.push(rank + suit))
   );
   return newDeck;
+};
+export const shuffleDeck = (deck: string[]): string[] => {
+  return [...deck].sort(() => 0.5 - Math.random());
 };
--- tests/deck.test.ts
+++ tests/deck.test.ts
@@ -1,14 +1,22 @@
-import { createDeckOfCards } from "../src/lib/deck";
+import { createDeckOfCards, shuffleDeck } from "../src/lib/deck";
+
+const deck = createDeckOfCards();
 
 describe("Generates a new deck of cards", () => {
-  const deck = createDeckOfCards();
   it("contains 52 cards", () => {
     expect(deck.length).toBe(52);
   });
 
   it("each card is unique", () => {
-    const isUnique = (arrToTest: any[]): boolean =>
-      arrToTest.length === new Set(arrToTest).size;
+    const isUnique = (arrayToTest: any[]): boolean =>
+      arrayToTest.length === new Set(arrayToTest).size;
     expect(isUnique(deck)).toBe(true);
+  });
+});
+
+describe("Shuffle cards", () => {
+  it("must be different from a new game", () => {
+    const deckShuffled = shuffleDeck(deck);
+    expect(deck).not.toBe(deckShuffled);
   });
 });
