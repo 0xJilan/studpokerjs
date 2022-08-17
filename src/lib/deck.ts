a50689354b1b2586a22cb6e8619c2c77f5bcc6ed
0xJilan <107534852+0xJilan@users.noreply.github.com>
2022-08-17 15:08:08 +0200

    refacto sortBySuitsAndValues

--- src/lib/deck.ts
+++ src/lib/deck.ts
@@ -1,18 +1,9 @@
 const suits: string[] = ["H", "C", "D", "S"];
 const values: number[] = [...Array(13)].map((_, index) => index + 1);
-const extractFromCard = (type: string, card: string): string => {
-  return type === "suits"
-    ? card.length > 2
-      ? card[2]
-      : card[1]
-    : type === "values" && card.length > 2
-    ? card[0] + card[1]
-    : card[0];
-};
 
 export const createDeckFromCards = (): string[] => {
   let newDeck: string[] = [];
-  suits.map((suit) => values.map((value) => newDeck.push(value + suit)));
+  suits.map((suit) => values.map((value) => newDeck.push(value + "-" + suit)));
   return newDeck;
 };
 
@@ -32,7 +23,7 @@ export const distributeDeck = (
 
 export const sortBySuitsAndValues = (deck: string[]): any[][] => {
   return [
-    deck.map((card) => extractFromCard("suits", card)).sort(),
-    deck.map((card) => extractFromCard("values", card)).sort(),
+    deck.map((card) => card.split("-")[1]).sort(),
+    deck.map((card) => card.split("-")[0]).sort(),
   ];
 };
